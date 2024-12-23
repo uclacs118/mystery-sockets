a8824dc98c965bcc47ae465f4b6a4423a45e53b2
burturt <31748545+burturt@users.noreply.github.com>
2024-12-23 14:00:35 -0800

    changes

--- player/socket.c
+++ player/socket.c
@@ -12,7 +12,34 @@ char cmd[65537];
 char ip_str[65537];
 char buf[65537];
 
-int main(){
+int tmp_errno;
+
+void lineSleep() {
+  usleep(100 * 1000);
+}
+
+void printStr(char* s) {
+  printf("$ printf(\"%s\\n\")\n< %s\n", s, s);
+  lineSleep();
+}
+
+void printCode(char* s) {
+  printf("$ %s\n", s);
+  lineSleep();
+}
+
+void printVal(int val, char* n) {
+  printf("$ printf(\"%s: %%d\\n\", %s)\n< %s: %d\n", n, n, n, val);
+  lineSleep();
+}
+
+void printErr() {
+  printf("%s", "$ printf(\"Error with code %d: %s\\n\", errno, strerror(errno));\n");
+  printf("< Error with code %d: %s\n", tmp_errno, strerror(tmp_errno));
+  lineSleep();
+}
+
+int main() {
   int sock = -1;
   struct sockaddr_in addr;
   socklen_t socklen;
@@ -20,22 +47,33 @@ int main(){
 
   setbuf(stdout, NULL);
 
+  printf("INITIALIZING NETSIFT TOOLCHAIN...\n");
+  printCode("int sock = -1;");
+  printCode("struct sockaddr_in addr;");
+  printCode("socklen_t socklen;");
+  printCode("int port, buflen;");
+  
+
   printf("INTERNAL NETSIFT TOOLCHAIN. Type help for available commands.\n");
+
+  //printErr(5);
+
 
   for(printf("> "); fgets(cmd, sizeof(cmd), stdin) != NULL; printf("> ")){
     if(strncmp(cmd, "exit", 4) == 0){
       break;
     } else if(strncmp(cmd, "socket", 6) == 0){
       // Print code
-      printf("sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);\n");
+      printCode("sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);");
 
       // Create socket: IPv4 family, Stream socket, TCP protocol
       // Document: https://man7.org/linux/man-pages/man2/socket.2.html
       sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-      printf("sock == %d\n", sock);
+      tmp_errno = errno;
+      printVal(sock, "sock");
       // -1 means error
-      if(sock == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+      if (sock == -1) {
+        printErr();
       }
     } else if(strncmp(cmd, "connect", 7) == 0) {
       if(sscanf(cmd, "connect %d %s %d", &sock, ip_str, &port) != 3){
@@ -45,30 +83,38 @@ int main(){
       }
 
       // Print code
-      printf("sockaddr_in addr;\n");
-      printf("addr.sin_family = AF_INET;\n");
-      printf("addr.sin_port = htons(%d);\n", port);
-      printf("inet_pton(AF_INET, %s, &addr.sin_addr);\n", ip_str);
+      //printf("$ sockaddr_in addr;\n");
+      //lineSleep();
+      printf("$ addr.sin_family = AF_INET;\n");
+      lineSleep();
+      printf("$ addr.sin_port = htons(%d);\n", port);
+      lineSleep();
+      printf("$ ret = inet_pton(AF_INET, %s, &addr.sin_addr);\n", ip_str);
+      lineSleep();
 
       // IPv4 family
       addr.sin_family = AF_INET;
       // Port number. Note that IP uses big-endian.
       addr.sin_port = htons(port);
       // Convert IP address from string to binary form
-      if(!inet_pton(AF_INET, ip_str, &addr.sin_addr)){
-        printf("Wrong IP address\n");
+      int ret = inet_pton(AF_INET, ip_str, &addr.sin_addr);
+      printVal(ret, "ret");
+      if(!ret) {
+        printStr("Invalid IP address");
         continue;
       }
 
       // Print code
-      printf("ret = connect(%d, (sockaddr*)&addr, sizeof(addr));\n", sock);
+      printf("$ ret = connect(%d, (sockaddr*)&addr, sizeof(addr));", sock);
+      lineSleep();
 
       // Connect to a server
       // Document: https://man7.org/linux/man-pages/man2/connect.2.html
-      int ret = connect(sock, (struct sockaddr*)&addr, sizeof(addr));
-      printf("ret == %d\n", ret);
-      if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+      ret = connect(sock, (struct sockaddr*)&addr, sizeof(addr));
+      tmp_errno = errno;
+      printVal(ret, "ret");
+      if(ret == -1) {
+        printErr();
       }
     } else if(strncmp(cmd, "bind", 4) == 0) {
       if(sscanf(cmd, "bind %d %s %d", &sock, ip_str, &port) != 3){
@@ -79,33 +125,45 @@ int main(){
       }
 
       // Print code
-      printf("sockaddr_in addr;\n");
-      printf("addr.sin_family = AF_INET;\n");
-      printf("addr.sin_port = htons(%d);\n", port);
+      //printf("$ struct sockaddr_in addr;\n");
+      //lineSleep();
+      printf("$ addr.sin_family = AF_INET;\n");
+      lineSleep();
+      printf("$ addr.sin_port = htons(%d);\n", port);
+      lineSleep();
 
       addr.sin_family = AF_INET;
       addr.sin_port = htons(port);
 
       if(strcmp(ip_str, "any") == 0){
-        printf("addr.sin_addr.s_addr = INADDR_ANY;\n");
+        printf("$ addr.sin_addr.s_addr = INADDR_ANY;\n");
+        lineSleep();
         addr.sin_addr.s_addr = htonl(INADDR_ANY);
       } else {
-        printf("inet_pton(AF_INET, %s, &addr.sin_addr);\n", ip_str);
-        if(!inet_pton(AF_INET, ip_str, &addr.sin_addr)){
-          printf("Wrong IP address\n");
+        printf("$ ret = inet_pton(AF_INET, %s, &addr.sin_addr);\n", ip_str);
+        int ret = inet_pton(AF_INET, ip_str, &addr.sin_addr);
+        lineSleep();
+        printVal(ret, "ret");
+        lineSleep();
+        if(!ret) {
+          printStr("Invalid IP address");
+          lineSleep();
           continue;
         }
       }
 
       // Print code
-      printf("ret = bind(%d, (sockaddr*)&addr, sizeof(addr));\n", sock);
+      printf("$ ret = bind(%d, (sockaddr*)&addr, sizeof(addr));\n", sock);
+      lineSleep();
 
       // Bind the address with the socket, as a server
       // Document: https://man7.org/linux/man-pages/man2/bind.2.html
       int ret = bind(sock, (struct sockaddr*)&addr, sizeof(addr));
-      printf("ret == %d\n", ret);
+      tmp_errno = errno;
+      printVal(ret, "ret");
+      // printf("ret == %d\n", ret);
       if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+        printErr();
       }
     } else if(strncmp(cmd, "listen", 6) == 0) {
       if(sscanf(cmd, "listen %d", &sock) != 1){
@@ -115,13 +173,15 @@ int main(){
       }
 
       // Print code
-      printf("ret = listen(%d, 0);\n", sock);
+      printf("$ ret = listen(%d, 0);\n", sock);
 
       // Listen to connection, with minimal backup queue
       int ret = listen(sock, 0);
-      printf("ret == %d\n", ret);
+      tmp_errno = errno;
+      printVal(ret, "ret");
+      // printf("ret == %d\n", ret);
       if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+        printErr();
       }
     } else if(strncmp(cmd, "accept", 6) == 0) {
       if(sscanf(cmd, "accept %d", &sock) != 1){
@@ -130,23 +190,47 @@ int main(){
         continue;
       }
 
-      // Print code
-      printf("socklen = sizeof(addr);\n");
-      printf("new_sock = accept(%d, (sockaddr*)&addr, &socklen);\n", sock);
 
+
+      // Print code
+      printf("$ socklen = sizeof(addr);\n");
+      lineSleep();
+      printf("$ new_sock = accept(%d, (sockaddr*)&addr, &socklen);\n", sock);
+      lineSleep();
+
+
+      fcntl(sock, F_SETFL, fcntl(sock, F_GETFL, 0) | O_NONBLOCK);
       // Accept a connection
       // Document: https://man7.org/linux/man-pages/man2/accept.2.html
       // addr will contain the client's IP address and port
       socklen = sizeof(addr);
+
+      
       int new_sock = accept(sock, (struct sockaddr*)&addr, &socklen);
-      printf("new_sock == %d\n", new_sock);
-      if(new_sock == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+      tmp_errno = errno;
+      printVal(new_sock, "new_sock");
+      if(new_sock == -1) {
+        printErr();
         continue;
       }
+      printCode("inet_ntop(AF_INET, &addr.sin_addr, ip_str, socklen);");
       inet_ntop(AF_INET, &addr.sin_addr, ip_str, socklen);
-      printf("Client IP address: %s port: %d\n", ip_str, ntohs(addr.sin_port));
-      printf("Now use socket %d to communicate with the client\n", new_sock);
+
+      printf("$ printf(\"IP: %%s\", ip_str);\n");
+      lineSleep();
+      printf("< IP: %s\n", ip_str);
+      lineSleep();
+
+      printf("$ printf(\"Port: %%d\", ntohs(addr.sin_port));\n");
+      lineSleep();
+      printf("< Port: %d\n", ntohs(addr.sin_port));
+      lineSleep();
+
+      // printVal("ntohs(addr.sin_port)", ntohs(addr.sin_port));
+      // printf("Client IP address: %s port: %d\n", ip_str, ntohs(addr.sin_port));
+      // printf("Now use socket %d to communicate with the client\n", new_sock);
+      printVal(new_sock, "new_sock");
+      printStr("Now use this new socket to communicate with the client");
     } else if(strncmp(cmd, "send", 4) == 0) {
       if(sscanf(cmd, "send %d %[^\n]", &sock, buf) != 2){
         printf("Usage: send SOCKET STRING\n");
@@ -155,18 +239,23 @@ int main(){
       }
 
       // Print code
-      printf("ret = send(%d, \"%s\", %lu, 0);\n", sock, buf, strlen(buf));
+      printf("$ ret = send(%d, \"%s\", %lu, 0);\n", sock, buf, strlen(buf));
 
       // Send data to the socket (you may also use write)
       // Document: https://man7.org/linux/man-pages/man2/send.2.html
       // Length is strlen(buf)
       // The last argument is flag, can be ignored for now
       int ret = send(sock, buf, strlen(buf), 0);
-      printf("ret == %d\n", ret);
+      tmp_errno = errno;
+
+
+      printVal(ret, "ret");
+      // printf("ret == %d\n", ret);
       if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+        printErr();
+        // printf("Error with code %d: %s\n", errno, strerror(errno));
       } else {
-        printf("Sent %d bytes\n", ret);
+        printVal(ret, "ret");
       }
     } else if(strncmp(cmd, "recv", 4) == 0) {
       if(sscanf(cmd, "recv %d %d", &sock, &buflen) != 2){
@@ -181,18 +270,24 @@ int main(){
       }
 
       // Print code
-      printf("ret = recv(%d, buf, %d, 0);\n", sock, buflen);
+      printf("$ ret = recv(%d, buf, %d, MSG_DONTWAIT);\n", sock, buflen);
 
       // Receive data from the socket (you may also use read)
       // Document: https://man7.org/linux/man-pages/man2/recv.2.html
       // The last argument is flag, can be ignored for now
-      int ret = recv(sock, buf, buflen, 0);
-      printf("ret == %d\n", ret);
+      int ret = recv(sock, buf, buflen, MSG_DONTWAIT);
+      tmp_errno = errno;
+      printVal(ret, "ret");
+      // printf("ret == %d\n", ret);
       if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+        printErr();
+        // printf("Error with code %d: %s\n", errno, strerror(errno));
       } else {
         buf[ret] = 0;  // Adding a tailing '\0' for print
-        printf("Received %d bytes\n", ret);
+        // printf("Received %d bytes\n", ret);
+        // printStr(buf)
+        printCode("buf[ret] = 0; // Add tailing \\0 for print");
+        printf("$ printf(\"buf: %%s\\n\")\n");
         printf("buf: %s\n", buf);
       }
     } else if(strncmp(cmd, "close", 5) == 0) {
@@ -203,13 +298,14 @@ int main(){
       }
 
       // Print code
-      printf("ret = close(%d);\n", sock);
-
+      printf("$ ret = close(%d);\n", sock);
+      lineSleep();
       // Shutdown socket
       int ret = close(sock);
-      printf("ret == %d\n", ret);
+      tmp_errno = errno;
+      printVal(ret, "ret");
       if(ret == -1){
-        printf("Error with code %d: %s\n", errno, strerror(errno));
+        printErr();
       }
       sock = -1;
     } else {
